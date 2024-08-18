442e4c09c7190c3a95ef5b676ca2487fef890a47
Adrian <adrian.dryzara3@gmail.com>
2024-08-17 22:59:35 -0400

    Responsive Design II

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const slider = document.querySelector("#price-range")
 let plan = document.getElementById("plan")
 let planArr = [
     `
-    <span class="price">300$</span>
-    <h3>Basico</h3>
+        <span class="price">300$</span>
+        <h3>Basico</h3>
     `,
     `
         <span class="price">600$</span>
@@ -36,7 +36,6 @@ function ajustarImagenes(){
     let minWidth = images[0].width;
 
     images.forEach(image => {
-    console.log(image);
     if (image.height < maxHeight) {
         maxHeight = image.height;
     }
@@ -58,7 +57,7 @@ slider.addEventListener("input", (event) => {
 
 ajustarImagenes()
 
-    
+
 function mostrarSwipper() {
     let swiper = document.querySelector('.swiper-wrapper');
     swiper.innerHTML = `
@@ -113,56 +112,97 @@ const swiper = new Swiper('.swiper', {
         delay: 2000,
         disableOnInteraction: false
     },
-        loop: true,
-        spaceBetween: 30,
-        slidesPerView: 5,
-        breakpoints: {
-            320: {
-                slidesPerView: 2,
-                spaceBetween: 5
-            },
-            580: {
-                slidesPerView: 2,
-                spaceBetween: 15
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 25
-            },
-            992: {
-                slidesPerView: 4,
-                spaceBetween: 25
-            },
-            1200: {
-                slidesPerView: 5,
-                spaceBetween: 25
-            },
-        }
-    });
-
-    function showBuyCard(i){
-        console.log(i);
-        let content = document.getElementsByClassName('card-price')
-        let title = document.getElementById('title')
-        let price = document.getElementById('price')
-        let description = document.getElementById('description')
-        switch(i){
-            case 1:
-                title.innerHTML = "Basico"
-                price.innerHTML = "300$"
-                description.innerHTML = "Plan básico con características esenciales"
-                break
-            case 2:
-                title.innerHTML = "Standart"
-                price.innerHTML = "600$"
-                description.innerHTML = "Plan estándar con características adicionales"
-                break
-            case 3:
-                title.innerHTML = "Premium"
-                price.innerHTML = "900$"
-                description.innerHTML = "Plan premium con todas las características y beneficios"
-                break
-        }
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        580: {
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 25
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 25
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 25
+        },
     }
+});
 
-    showBuyCard(1)
+function showBuyCard(i){
+    let title = document.getElementById('title')
+    let price = document.getElementById('price')
+    let description = document.getElementById('description')
+
+    let title2 = document.getElementById('title2')
+    let price2 = document.getElementById('price2')
+    let description2 = document.getElementById('description2')
+
+    let title3 = document.getElementById('title3')
+    let price3 = document.getElementById('price3')
+    let description3 = document.getElementById('description3')
+
+    switch(i){
+        case 1:
+            title.innerHTML = "Basico"
+            price.innerHTML = "300$"
+            description.innerHTML = "Plan básico con características esenciales"
+            break
+        case 2:
+            title.innerHTML = "Standart"
+            price.innerHTML = "600$"
+            description.innerHTML = "Plan estándar con características adicionales"            
+            break
+        case 3:
+            title.innerHTML = "Premium"
+            price.innerHTML = "900$"
+            description.innerHTML = "Plan premium con todas las características y beneficios"
+            break
+        case 4:
+            title2.innerHTML = "Basico"
+            price2.innerHTML = "300$"
+            description2.innerHTML = "Plan básico con características esenciales"
+            break
+        case 5:
+            title2.innerHTML = "Standart"
+            price2.innerHTML = "600$"
+            description2.innerHTML = "Plan estándar con características adicionales"
+            break
+        case 6:
+            title2.innerHTML = "Premium"
+            price2.innerHTML = "900$"
+            description2.innerHTML = "Plan premium con todas las características y beneficios"
+            break
+        case 7:
+            title3.innerHTML = "Basico"
+            price3.innerHTML = "300$"
+            description3.innerHTML = "Plan básico con características esenciales"
+            break
+        case 8:
+            title3.innerHTML = "Standart"
+            price3.innerHTML = "600$"
+            description3.innerHTML = "Plan estándar con características adicionales"
+            break
+        case 9:
+            title3.innerHTML = "Premium"
+            price3.innerHTML = "900$"
+            description3.innerHTML = "Plan premium con todas las características y beneficios"
+            break
+    }
+}
+
+// Menu
+function toggleMenu(){
+    let menu = document.querySelector('.menu-responsive');
+    menu.classList.toggle('active');
+}
